a081693e0e549b521e7bce8dd0b7f53bae2d282d
Brittany Halterman <halterma@gmail.com>
2025-03-02 14:47:16 -0500

    Retrieve and parse data

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 // build your own dictionary app
+require('dotenv').config();
+
+const api_key = process.env.API_KEY;
+
+// console.log(api_key);
 
 // Require https module
 const https = require('https');
@@ -7,7 +12,27 @@ function getDef(term) {
   try {
     // request data
     const request = https.get(
-      `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${term}?key=your-api-key`
+      `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${term}?key=${api_key}`,
+      (response) => {
+        let body = '';
+        // Read the data
+        response.on('data', (data) => {
+          body += data.toString();
+        });
+
+        response.on('end', () => {
+          // Parse the data
+          const definition = JSON.parse(body);
+          // Print the data
+          console.log(definition[0].shortdef);
+        });
+      }
     );
-  } catch (error) {}
+    request.on('error', (error) => console.error(error.message));
+  } catch (error) {
+    console.error(error.message);
+  }
 }
+
+const query = process.argv.slice(2);
+query.forEach(getDef);
