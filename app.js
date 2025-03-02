dd1de4776b991189e87c77539bfac8096721a0d1
Brittany Halterman <halterma@gmail.com>
2025-03-02 14:36:24 -0500

    Add .gitignore

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // build your own dictionary app
+
+// Require https module
+const https = require('https');
+
+function getDef(term) {
+  try {
+    // request data
+    const request = https.get(
+      `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${term}?key=your-api-key`
+    );
+  } catch (error) {}
+}
